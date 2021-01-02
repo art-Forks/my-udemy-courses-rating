559e4070534da389ae1bf625a21efb3d3f1da472
art <partypeopleland@gmail.com>
2021-01-02 20:05:12 +0800

    增加評等數量、註冊人數，刪除失效的事件註冊

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
+## Intro
+
+調整說明:
+
+1. 增加`評等數`, `註冊數`
+1. 刪除已經無效的註冊事件
+
+已知問題:
+
+1. 點連結後無法重新爬資訊，需要重新整理頁面
+1. 目前抓取資訊比對採用`regex`，未來可能遇到UI改版就掛掉
+
+備註:
+有興趣的自行取用抓去改
+
+## [原作Github](https://github.com/0t2/my-udemy-courses-rating) 以下為原開發者 Readme.md
+
 非開發者:
 
 1. 前往 [release](https://github.com/0t2/my-udemy-courses-rating/releases) 下載 build.zip
 
 2. 解壓縮並參考[這篇文章](https://ithelp.ithome.com.tw/articles/10156599)載入套件
+
 ---
 開發者:
+
 ```
 npm i
 npm run build
 ```
+
 ---
 預覽如下:
+
 1. 分數會顯示在紅框處，請耐心等待分數出現
 2. 如果沒有分數，可能是**課程不開放註冊**或被認為是**機器人**了
 
@@ -18,6 +39,7 @@ npm run build
 
 ---
 程式作法簡介:
+
 1. 套件只會作用在 https://www.udemy.com/home/my-courses/ 底下的網頁
 2. 抓出頁面所有的網址
 3. 依序前往介紹頁面抓分數，最多會連發 12 個請求
--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,86 +1,67 @@
 import axios from "axios";
 
-window.addEventListener("load", function () {
-  document
-    ?.getElementsByClassName("previous")[0]
-    ?.addEventListener("click", () => {
-      createInterval();
-    });
-  document?.getElementsByClassName("next")[0]?.addEventListener("click", () => {
-    createInterval();
-  });
-
-  let interval = setInterval(() => {
-    if (document.querySelectorAll(".nav-slide li").length > 0) {
-      document.querySelectorAll(".nav-slide li").forEach((element, key) => {
-        if (key !== 2) {
-          element.addEventListener("click", () => {
-            createInterval();
-          });
-        }
-      });
-      clearInterval(interval);
-    }
-  }, 500);
-});
-
 const createInterval = () => {
-  let intervalId = setInterval(() => {
-    if (document.getElementsByClassName("card--learning__image").length > 0) {
-      const urls = Array.from(
-        document.getElementsByClassName("card--learning__image")
-      );
+    let intervalId = setInterval(() => {
+        if (document.getElementsByClassName("card--learning__image").length > 0) {
+            const urls = Array.from(document.getElementsByClassName("card--learning__image"));
 
-      urls.forEach((url, index) => {
-        setTimeout(() => {
-          let redirectUrl = (<HTMLLinkElement>url).href;
-          let regex = /course_id=(.+)/gm;
-          let match = regex.exec(redirectUrl);
-          if (match !== null) {
-            axios
-              .get(`https://www.udemy.com/course/${match[1]}`)
-              .then((res) => {
-                if (res.request.responseURL) {
-                  axios.get(res.request.responseURL).then((res) => {
-                    if (
-                      (<string>res.data).includes(
-                        'data-purpose="rating-number">'
-                      )
-                    ) {
-                      regex = /data-purpose="rating-number">(.*)<\/span><svg/gm;
-                    } else if (
-                      (<string>res.data).includes('id="rate-count-value')
-                    ) {
-                      regex = /id="rate-count-value.*">(.*)<\/span>/gm;
+            urls.forEach((url, index) => {
+                setTimeout(() => {
+                    let redirectUrl = (<HTMLLinkElement>url).href;
+                    let regex = /course_id=(.+)/gm;
+                    let match = regex.exec(redirectUrl);
+
+                    const regexRules: { [index: string]: any } = {
+                        score: {
+                            word: '星',
+                            keyword: 'data-purpose="rating-number">',
+                            pattern: /data-purpose="rating-number">(.*)<\/span><svg/gm
+                        },
+                        count: {
+                            word: '個評等',
+                            keyword: '個評等',
+                            pattern: / \((.*) 個評等\)/gm
+                        },
+                        student: {
+                            word: '位學生',
+                            keyword: 'data-purpose="enrollment" class="">',
+                            pattern: /data-purpose="enrollment" class="">\n(.*) 位學生/gm
+                        }
                     }
 
-                    match = regex.exec(res.data);
+
+                    function appendMessage(message: any[]) {
+                        if (message.length > 0) {
+                            const appendMessage = message.join(' ')
+                            document.getElementsByClassName("card--learning__details")[index].getElementsByClassName("details__start-course")[0]?.insertAdjacentText("beforeend", ` - ${appendMessage}`);
+                            document.getElementsByClassName("card--learning__details")[index].getElementsByClassName("ellipsis")[0]?.insertAdjacentText("beforeend", ` - ${appendMessage}`);
+                        }
+                    }
+
+                    function prepareMessage(regexRules: { [p: string]: any }, context: string) {
+                        let result = []
+                        for (const key in regexRules) {
+                            let pattern = context.includes(regexRules[key].keyword) ? regexRules[key].pattern : null
+                            let match = pattern ? pattern.exec(context) : null
+                            if (match !== null) result.push(`${match[1].trim()}${regexRules[key].word}`)
+                        }
+                        return result;
+                    }
+
                     if (match !== null) {
-                      document
-                        .getElementsByClassName("card--learning__details")
-                        [index].getElementsByClassName(
-                          "details__start-course"
-                        )[0]
-                        ?.insertAdjacentText(
-                          "beforeend",
-                          ` - ${match[1].trim()}`
-                        );
-                      document
-                        .getElementsByClassName("card--learning__details")
-                        [index].getElementsByClassName("ellipsis")[0]
-                        ?.insertAdjacentText(
-                          "beforeend",
-                          ` - ${match[1].trim()}`
-                        );
+                        let targetUrl = `https://www.udemy.com/course/${match[1]}`
+                        axios.get(targetUrl).then((res) => {
+                            if (res.request.responseURL) {
+                                axios
+                                    .get(res.request.responseURL)
+                                    .then(res => appendMessage(prepareMessage(regexRules, res.data)))
+                            }
+                        });
                     }
-                  });
-                }
-              });
-          }
-        }, index * 100);
-      });
-      clearInterval(intervalId);
-    }
-  }, 500);
+                }, index * 100);
+            });
+            clearInterval(intervalId);
+        }
+    }, 500);
 };
 createInterval();
